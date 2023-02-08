101b05ed47599a4a1fe9c58b1a67441b8c027592
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-08 13:43:11 +0000

    Iteration 1

--- main.c
+++ main.c
@@ -3,9 +3,21 @@
   Area of a Triangle
   Max Gombos
 */
-#include <stdio.h>
+#include <stdio.h> //import input and output functions
 
-int main(void) {
+int main(void) { //define main function
+  float triangleBase, triangleHeight, triangleArea;
+  //^initialize variables as float
+
+  printf("Triangle Base: "); //ask user for triangle base
+  scanf("%f", &triangleBase); //accepts user input, format as float
+  printf("Triangle Height: "); //ask user for triangle height
+  scanf("%f", &triangleHeight); //accepts user input, format as float
+
+  triangleArea = 0.5 * (triangleBase * triangleHeight);
+  //^find area of triangle
+
+  printf("Area: %0.2f", triangleArea); //output area
   
   return 0;
 }
