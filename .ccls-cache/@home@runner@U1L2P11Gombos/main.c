0f06b0a806e16f630c2de66ccee690cd1a49d56c
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-08 13:55:12 +0000

    Iteration 2

--- .ccls-cache/@home@runner@U1L2P11Gombos/main.c
+++ .ccls-cache/@home@runner@U1L2P11Gombos/main.c
@@ -9,15 +9,17 @@ int main(void) { //define main function
   float triangleBase, triangleHeight, triangleArea;
   //^initialize variables as float
 
-  printf("Triangle Base: "); //ask user for triangle base
+  printf("This program allows you to enter the base and height of a triangle. The program will then output the area of this triangle. You will get two prompts:\n\t- Base: Enter the base of the triangle\n\t- Height: Enter the height of the triangle\n"); //Intructions
+  
+  printf("Base: "); //ask user for triangle base
   scanf("%f", &triangleBase); //accepts user input, format as float
-  printf("Triangle Height: "); //ask user for triangle height
+  printf("Height: "); //ask user for triangle height
   scanf("%f", &triangleHeight); //accepts user input, format as float
 
   triangleArea = 0.5 * (triangleBase * triangleHeight);
   //^find area of triangle
 
-  printf("Area: %0.2f", triangleArea); //output area
+  printf("The area of your triangle is %0.2f units squared.", triangleArea); //output area
   
   return 0;
 }
